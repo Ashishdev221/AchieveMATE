4ef35586bc918e78f89386a2ede7662b316ddead
Pankaj Kumawat <pankajkumawat.610@gmail.com>
2023-04-16 16:20:24 +0530

    sign up  and login page updated

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
 import Components from "./pages/Components";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/login" element={[<LoginPage />]} />
-          <Route path="/comp" element={[<Components />]} />
           <Route path="/" element={[<LandingPage />]} />
+          <Route path="/login" element={[<LoginPage />]} />
+          <Route path="/Signup" element={[<SignUpPage />]} />
+          <Route path="/comp" element={[<Components />]} />
         </Routes>
       </div>
     </Router>
