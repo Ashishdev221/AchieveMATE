320bd924f0c37a64a9172bf27ab6c61fe35f7ea2
Kanhav Sukhwal <kanhavsukhwal@gmail.com>
2023-04-17 10:56:42 +0530

    resolved button conflict

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import "./Button.css";
 
-<<<<<<< HEAD
 function Button({label,id,className }) {
   return (
     <div className={className?className:'button'} id={id?id:''}>
         <a href="">{label}</a>
-=======
-function Button(props) {
-  return (
-    <div className= {'button ' +props.className}>
-        <a href="">{props.name}</a>
->>>>>>> e3728284e69f68b60f007ea85f844f1da8e7a291
     </div>
   )
 }
